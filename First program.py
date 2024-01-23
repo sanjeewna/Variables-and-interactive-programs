ea0dccf9d5db9f0a7fe4700746136f60fdbef8e3
sanjeewna <sanjeewnadevi93@gmail.com>
2024-01-23 09:44:58 +0200

    Variables and interactive programs(1,2,3and 4 program completed)

---
+++ First program.py
@@ -0,0 +1,3 @@
+user_name = input("Enter your name: ")
+#Greet the user
+print(f"Hello, {user_name}!")
---
+++ Fourth Program.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter the first integer: "))
+num2 = int(input("Enter the second integer: "))
+num3 = int(input("Enter the third integer: "))
+sum = num1+num2+num3
+product = num1*num2*num3
+average = (num1 + num2 + num3) / 3
+print(f"Sum of the numbers: {sum}")
+print(f"Product of the numbers: {product}")
+print(f"Average of the numbers: {average}")
